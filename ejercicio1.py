2585619f6e27152478f3c0b6aca9e40095c9f73b
samu9809 <79594592+samu9809@users.noreply.github.com>
2021-03-23 10:47:54 -0500

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,10 @@
+contador = 0
+while(contador <= 100):
+    contador = contador + 1
+    result = contador * contador 
+    print("El cuadrado de " + str(contador) + " es: " + str(result))
+
+
+for a in range(100):
+    result = a * a 
+    print("El cuadrado de " + str(a) + " es: " + str(result))
