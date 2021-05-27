cb9f82244161e51e3a8be1b7ba2d2a8047348f16
samu9809 <79594592+samu9809@users.noreply.github.com>
2021-05-27 08:10:38 -0500

    Add files via upload

---
+++ ordenamiento por insercion.py
@@ -0,0 +1,15 @@
+def ordenamientoPorInsercion(unaLista):
+   for indice in range(1,len(unaLista)):
+
+     valorActual = unaLista[indice]
+     posicion = indice
+
+     while posicion>0 and unaLista[posicion-1]>valorActual:
+         unaLista[posicion]=unaLista[posicion-1]
+         posicion = posicion-1
+
+     unaLista[posicion]=valorActual
+
+unaLista = [54,26,93,17,77,31,44,55,20]
+ordenamientoPorInsercion(unaLista)
+print(unaLista)
